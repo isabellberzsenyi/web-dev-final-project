7aaea65400c45853b0d09035a5518452c2484e24
Karen Zhu <zkaren2001@gmail.com>
2022-04-21 17:05:37 -0400

    updated home page

--- find-your-meal/src/components/Home.jsx
+++ find-your-meal/src/components/Home.jsx
@@ -1,24 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import example from '../service/service';
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 import NavBar from './NavBar';
+import './Home.css';
 
 function Home() {
-  const [apiText, setApiText] = useState();
-  const apiExample = async () => {
-    await example().then((r) => {
-      setApiText(r.data);
-    });
+  const API_URL = 'https://www.themealdb.com/api/json/v1/1/random.php';
+  const [theMeals, setMeals] = useState([]);
+  const nameRef = useRef();
+
+  const getMostLikedMeals = async () => {
+    const response = await axios.get(`${API_URL}=${nameRef.current.value}`);
+    setMeals(response.data.meals);
   };
 
   useEffect(() => {
-    apiExample();
+    getMostLikedMeals();
   }, []);
+
   return (
     <>
       <NavBar currentPage='home' />
-      <h1>Home Page</h1>
+      <div className='banner center'>
+        <div>
+          <h1> <b> Find Your Meal </b> </h1>
+          <form className='center'>
+            <input type='text' value='' />
+            <input type='button' value='Search' />
+          </form>
+        </div>
+      </div>
       <div>
-        <p>from API: {apiText}</p>
+        <h3> Most Liked Meals </h3>
+        <div className='row'>
+          {
+            theMeals.map((meal) => (
+              <div className="card col-4" key={meal.idMeal}>
+                <Link to={`/details/${meal.idMeal}`}>
+                  <img src={meal.strMealThum} alt="most liked meal" />
+                  <div className="container">
+                    <h4>{meal.strMeal}</h4>
+                  </div>
+                </Link>
+              </div>
+            ))
+          }
+        </div>
       </div>
     </>
   );
