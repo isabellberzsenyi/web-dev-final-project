fb94fb809eb8ab0cd13b85fa36165b5898caf510
Izzy Berzsenyi <iberzsenyi@yahoo.com>
2022-04-22 18:29:30 -0400

    update navbar to only show login/signup when no profile is being used, and add profile provider to the app

--- find-your-meal/src/App.jsx
+++ find-your-meal/src/App.jsx
@@ -4,17 +4,22 @@ import './vendors/fontawesome/css/all.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
+import SignUp from './components/SignUp';
+import { ProfileProvider } from './contexts/profile-context';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className='container'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <ProfileProvider>
+      <BrowserRouter>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<SignUp />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </ProfileProvider>
   );
 }
 
